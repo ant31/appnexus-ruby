85c0381e3de47075e1534aa99ea87e8be02d0e4b
Antoine Legrand <antoine.legrand@arkena.com>
2015-08-17 17:20:14 +0200

    update segment

--- lib/appnexus/models/segment.rb
+++ lib/appnexus/models/segment.rb
@@ -4,24 +4,24 @@ module AppNexus
       include SimpleModel::Association
       auto_define_attributes false
       auto_find_relations true
-      override_attributes  [:code,
-                            :state,
-                            :short_name,
-                            :description,
-                            :member_id,
-                            :category,
-                            :price,
-                            :expire_minutes,
-                            :enable_rm_piggyback,
-                            :max_usersync_pixels,
-                            :advertiser_id,
-                            :last_modified,
-                            :provider,
-                            :parent_segment_id,
-                            :piggyback_pixels,
-                            :querystring_mapping,
-                            :querystring_mapping_key_value
-                           ]
+      override_attributes  :code,
+      :state,
+      :short_name,
+      :description,
+      :member_id,
+      :category,
+      :price,
+      :expire_minutes,
+      :enable_rm_piggyback,
+      :max_usersync_pixels,
+      :advertiser_id,
+      :last_modified,
+      :provider,
+      :parent_segment_id,
+      :piggyback_pixels,
+      :querystring_mapping,
+      :querystring_mapping_key_value
+
     end
   end
 end
