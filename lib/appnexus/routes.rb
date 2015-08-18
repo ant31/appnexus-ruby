98b53754257af2f0dc75c0792f579d3d75bb4cd2
Antoine Legrand <antoine.legrand@arkena.com>
2015-08-19 00:48:29 +0200

    Remove body

--- lib/appnexus/routes.rb
+++ lib/appnexus/routes.rb
@@ -41,7 +41,7 @@ module AppNexus
       def generate(client=AppNexus::Client)
         OpenAPI::Route.draw(client) do
           ## AUTH
-          match "auth", "app_nexus/handlers/app_response#auth_response_model", :auth, via: :post, :body => AppNexus::Models::Auth, :to_json => {root: true}, :extra => {:skip_auth => true}
+          match "auth", "app_nexus/handlers/app_response#auth_response_model", :auth, via: :post, :options => {:skip_auth => true}
           ## Reports
           match "report", "app_nexus/handlers/app_response#report_model", :getReports, via: :get, :params => P.create(P.pagination, {:advertiser_id => Integer})
           ## Advertisers
@@ -50,6 +50,7 @@ module AppNexus
           # Segements
           match "segment", "app_nexus/handlers/app_response#response_model", :getSegments, via: :get, :params => P.create(P.pagination)
           match "segment", "app_nexus/handlers/app_response#response_model", :getSegment, via: :get, :params => {:id => Integer}
+          match "segment", "app_nexus/handlers/app_response#response_model", :createSegment, :via => :post
           # CRUD CALLS
           match "GET", "app_nexus/handlers/app_response#response_model", :GET, via: :get, :params => P.create(P.pagination)
           match "DELETE", "app_nexus/handlers/app_response#response_model", :DELETE, via: :get
